e5d4d8acd6fbb7006f0d038e1e71f85a8c0d10bc
dansusman <frenchminister@gmail.com>
2022-11-30 15:48:11 -0500

    Make New York Great Again

--- web/src/locations/locations.js
+++ web/src/locations/locations.js
@@ -1,13 +1,13 @@
 const cities = [
-  "London",
-  "Boston",
-  "Amsterdam",
-  "Rio de Janeiro",
-  "Paris",
-  "Beijing",
-  "Cape Town",
-  "Sofia",
-  "New York City",
+    "London",
+    "Boston",
+    "Amsterdam",
+    "Rio de Janeiro",
+    "Paris",
+    "Beijing",
+    "Cape Town",
+    "Sofia",
+    "New York",
 ];
 
 export default cities;
