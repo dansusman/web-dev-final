485e1ca46d71eabb55d062044c747ecb49ef44b9
dansusman <frenchminister@gmail.com>
2022-12-07 12:53:48 -0500

    Make follower/following buttons clearer

--- web/src/components/user-list-item.js
+++ web/src/components/user-list-item.js
@@ -1,12 +1,13 @@
 import { Button, HStack, Spacer } from "@chakra-ui/react";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const UserListItem = ({ user, followed, showFollowButton }) => {
     const username = followed ? user.followed.username : user.follower.username;
     const uid = followed ? user.followed._id : user.follower._id;
+    const nav = useNavigate();
     return (
         <HStack>
-            <Link to={`/profile/${uid}`}>{username}</Link>
+            <Button onClick={() => nav(`/profile/${uid}`)}>{username}</Button>
             <Spacer />
 
             {showFollowButton && (
